e312feb3aeec01b834119890d8eeacf27806b824
byKenVi <157965828+byKenVi@users.noreply.github.com>
2024-01-28 20:16:19 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("modifier-btn").addEventListener("click", function() {
+    alert("Fonction de modification à implémenter !");
+    // Ajoutez ici le code pour modifier les éléments de l'emploi du temps
+});
